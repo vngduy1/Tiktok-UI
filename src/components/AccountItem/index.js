55dcdda80630e86c27df16b4cd45b2afbd49f4f6
Duy <duyvu372001@gmail.com>
2024-03-15 16:49:45 +0900

    LogicHeader

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,13 +2,14 @@ import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
                 src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/4b85df94ddbc913a995d4d721e417580.jpeg?lk3s=a5d48078&x-expires=1710403200&x-signature=br8dJZVqzy%2BBpR5QEsIf8RJ8nt4%3D"
                 alt=""
@@ -16,7 +17,10 @@ function AccountItem() {
             <div className={cx('info')}>
                 <p className={cx('name')}>
                     <span>Nguyễn Văn Anh</span>
-                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                    <FontAwesomeIcon
+                        className={cx('check')}
+                        icon={faCheckCircle}
+                    />
                 </p>
                 <span className={cx('username')}>nguyenvana</span>
             </div>
