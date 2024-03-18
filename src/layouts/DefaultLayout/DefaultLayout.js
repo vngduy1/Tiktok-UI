8386159f7255e5ce43051290ffdd9972c542e207
Duy <duyvu372001@gmail.com>
2024-03-18 15:24:20 +0900

    SideBar

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import Header from '~/layouts/components/Header';
-import Sidebar from './SideBar';
+import Sidebar from '../components/SideBar';
 import classNames from 'classnames/bind';
 import styles from './DefaultLayout.module.scss';
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,9 @@ import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
+import Friends from '~/pages/Friends';
+import Explore from '~/pages/Explore';
+import Live from '~/pages/Live';
 import { HeaderOnly } from '~/layouts';
 
 import config from '~/config';
@@ -18,6 +21,18 @@ const publicRoutes = [
     {
         path: config.routes.profile,
         component: Profile,
+    },
+    {
+        path: config.routes.friends,
+        component: Friends,
+    },
+    {
+        path: config.routes.explore,
+        component: Explore,
+    },
+    {
+        path: config.routes.live,
+        component: Live,
     },
     {
         path: config.routes.upload,
